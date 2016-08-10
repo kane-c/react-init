2837d0230af2ae8e3dbd14fc247ab378bc1a1f8c
Kane <omgsif@gmail.com>
2016-08-10 20:57:58 +1000

    Enable webpack tree shaking
    Disable Babel modules (except for in `npm start`)

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,6 +1,6 @@
-import path from 'path';
+const path = require('path');
 
-import webpack from 'webpack';
+const webpack = require('webpack');
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
