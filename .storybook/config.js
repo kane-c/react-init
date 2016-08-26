7813932447cc8bacd6c9cacf05263bd9d14628e2
Kane <omgsif@gmail.com>
2016-08-26 19:39:39 +1000

    Fix tests with Storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -2,5 +2,5 @@ import { configure } from '@kadira/storybook';
 
 /* eslint-disable global-require */
 configure(() => {
-  require('../src/components/stories');
+  require('../src/stories');
 }, module);
