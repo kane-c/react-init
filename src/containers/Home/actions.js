f4ee8c095390122a0e7f1d06c854bb02be376894
Kane <omgsif@gmail.com>
2016-10-12 09:52:32 +1100

    Fix lint errors

--- src/containers/Home/actions.js
+++ src/containers/Home/actions.js
@@ -2,7 +2,7 @@ import { action } from 'containers/App/actions';
 
 import { REPOS } from './constants';
 
-export const repos = {
+export const repos = { // eslint-disable-line import/prefer-default-export
   failure: error => action(REPOS.FAILURE, { error }),
   request: username => action(REPOS.REQUEST, { username }),
   success: repos => action(REPOS.SUCCESS, { repos }),
--- src/containers/Home/constants.js
+++ src/containers/Home/constants.js
@@ -1,3 +1,3 @@
 import { createRequestTypes } from 'containers/App/actions';
 
-export const REPOS = createRequestTypes('app/Home/REPOS');
+export const REPOS = createRequestTypes('app/Home/REPOS'); // eslint-disable-line max-len, import/prefer-default-export
