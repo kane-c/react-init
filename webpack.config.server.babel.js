62f22eaa17c530dad86e817a8e2dd122df58ac73
Kane <kane-c@users.noreply.github.com>
2016-10-25 15:49:16 +1100

    Add favicons for all platforms
    Also refactored static assets to be served from the root
    rather than a subdirectory (prepa

--- webpack.config.server.babel.js
+++ webpack.config.server.babel.js
@@ -4,6 +4,13 @@ config.entry[1] = './src/index.jsx';
 config.output.filename = '../server.js';
 config.target = 'node';
 
+// Don't emit static files during the server build; only during the client
+// build. This prevents writing the assets twice.
+// Unless in development mode, where the client build is only made in memory
+if (process.env.NODE_ENV === 'production') {
+  config.module.rules[4].loader += '&emitFile=false';
+}
+
 if (process.env.NODE_ENV === 'development') {
   /* eslint-disable global-require */
   const nodeExternals = require('webpack-node-externals');
