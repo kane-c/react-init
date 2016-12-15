907abcbe9fdb2ccad00b4ab7fbe95986f91a71fd
Kane <kane-c@users.noreply.github.com>
2016-12-15 19:52:35 +1100

    Exclude the Redux dev tools from production

--- src/common.jsx
+++ src/common.jsx
@@ -34,7 +34,7 @@ export function getStore(preloadedState, routerMiddleware) {
   middleware = applyMiddleware(...middleware);
 
   /* eslint-disable no-underscore-dangle */
-  if (typeof window !== 'undefined' &&
+  if (process.env.NODE_ENV !== 'production' && typeof window !== 'undefined' &&
         window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) {
     middleware = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__(middleware);
   }
